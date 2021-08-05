b0620facd220c62f287de7b10d869f344e1b9729
Ufuk ARSLAN <>
2021-08-05 12:40:08 +0300

    asset auth changes

--- apps/server/routes/asset/router.js
+++ apps/server/routes/asset/router.js
@@ -11,29 +11,29 @@ const {
 const assetsRouter = express.Router();
 const assetsController = new AssetsController();
 
-assetsRouter.post('/', addAssetValidator, (req, res, next) => {
-	assetsController.addAsset(req, res, next); 
-});
-
-assetsRouter.post('/login', authenticateAssetValidator, (req, res, next) => {
-	assetsController.authenticateAsset(req, res, next);
-});
+// assetsRouter.post('/login', authenticateAssetValidator, (req, res, next) => {
+// 	assetsController.authenticateAsset(req, res, next);
+// });
 
 // JWT protection for the endpoints below
 const jwtService = new JwtService();
-const assetJwtVerifier = jwtService.verificationMiddlewareFactory('ASSET');
+const assetJwtVerifier = jwtService.verificationMiddlewareFactory('USER');
 assetsRouter.use(assetJwtVerifier);
 
+assetsRouter.post('/', addAssetValidator, (req, res, next) => {
+	assetsController.addAsset(req, res, next);
+});
+
 assetsRouter.get('/', getAssetValidator, (req, res, next) => {
-	assetsController.getAsset(req, res, next); 
+	assetsController.getAsset(req, res, next);
 });
 
 assetsRouter.patch('/', updateAssetValidator, (req, res, next) => {
-	assetsController.updateAsset(req, res, next); 
+	assetsController.updateAsset(req, res, next);
 });
 
 assetsRouter.post('/set-intermediary', (req, res, next) => {
-	assetsController.setIntermediaryState(req, res, next); 
+	assetsController.setIntermediaryState(req, res, next);
 });
 
 module.exports = assetsRouter;
